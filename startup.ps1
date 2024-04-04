e2509f0c66851336510a46ffbd5a071b93c0dcef
SunRey Eagle <seagle@enstoa.com>
2024-04-04 10:21:28 -0600

    Adding the startup.ps1 file for use with Powershell ISE.

---
+++ startup.ps1
@@ -0,0 +1,13 @@
+### RestShell Script ###
+###
+### Personal project to make a series of Rest Requests using Powershell. The idea being to make Rest Requests available to the layman while simplifying dev work.
+### Open this file in Powershell ISE to leverage the other scripts in this directory
+### By Default, this script will call up the command line menu that shows the different available files and scripts to run.
+
+.\menu.ps1
+
+
+### For those who hack
+
+# If you know already which request file to open and run, uncomment this line and comment the .\menu.ps1 line and provide the filename of your JSON file
+# .\InvokeRequest.ps1 -TargetRequestFile .\my_local_file.json
