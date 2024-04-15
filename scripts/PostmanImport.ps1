b0e50e36c16d40e0ed824922b9a619e80ca4c6bd
SunRey Eagle <seagle@enstoa.com>
2024-04-14 21:52:38 -0600

    Okay, now the Postman import script actually reads from the input file.

--- scripts/PostmanImport.ps1
+++ scripts/PostmanImport.ps1
@@ -7,3 +7,24 @@ param(
 
 
 $OutputFile = ",,_Postman_import_!!.json"
+
+
+### Read in the Request details from the selected input file: ##############################
+clear
+echo '###'
+
+$StartTimeNote = "### Start Time: " + (Get-Date).ToString()
+$StartTimeNote
+
+echo '###'
+echo ''
+# echo '### Moving to script folder: ' $PSScriptRoot
+# cd $PSScriptRoot
+
+echo '### Opening File:'
+$TargetRequestFile = ".\" + $TargetRequestFile
+$TargetRequestFile
+echo '' # Blank line
+
+
+$InputJSON = Get-Content $TargetRequestFile -Raw | ConvertFrom-Json
