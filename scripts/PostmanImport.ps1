e9034b68068c746b7d46b55387a5b8a04057aa60
SunRey Eagle <seagle@enstoa.com>
2024-04-14 21:51:13 -0600

    Added a new Script to handle Postman Import feature. Right now this script justs opens the input json file for reading.

---
+++ scripts/PostmanImport.ps1
@@ -0,0 +1,9 @@
+### Read in command line inputs: ##############################
+
+param(
+    [Parameter()]
+    [String]$TargetRequestFile = "001_Simple_example.json"
+)
+
+
+$OutputFile = ",,_Postman_import_!!.json"
